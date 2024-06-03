94dc51ff1117f84e763cdbf8e0c6415c5df417e0
Dylan Lecocq <dylan.lecocq.etu@univ-lille.fr>
2024-06-03 11:17:29 +0200

    Ajout de la doc pour les exceptions personnalisées

--- src/InvalidStructureException.java
+++ src/InvalidStructureException.java
@@ -1,4 +1,18 @@
+/**
+ * Signale que la structure du fichier parcouru est invalide.
+ */
 public class InvalidStructureException extends Exception {
+    /**
+     * Construit une nouvelle InvalidStructureException sans message détaillé.
+     */
     InvalidStructureException() {}
-    InvalidStructureException(String msg) {super(msg);}
+
+    /**
+     * Construit une nouvelle InvalidStructureException avec le message détaillé spécifié.
+     * 
+     * @param msg le message détaillé
+     */
+    InvalidStructureException(String msg) {
+        super(msg);
+    }
 }
