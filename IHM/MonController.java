4db6fc22a1be23c7fa527e3bd898129cb109b015
Dylan Lecocq <dylan.lecocq.etu@univ-lille.fr>
2024-06-11 18:57:04 +0200

    Premiere version fonctionnelle de l'IHM

--- IHM/MonController.java
+++ IHM/MonController.java
@@ -1,6 +1,10 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import fr.ulille.but.sae_s2_2024.*;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -48,6 +52,8 @@ public class MonController {
     private CheckBox checkBoxBus;
     @FXML
     private ComboBox<TypeCout> criteres;
+    @FXML
+    private Button boutonCalculerVoyage;
 
     // Historique
     @FXML
@@ -60,6 +66,8 @@ public class MonController {
     private TextField nom;
     @FXML
     private TextField prenom;
+    @FXML
+    private TextField coutMax;
     @FXML
     private CheckBox profilCheckBoxTrain;
     @FXML
@@ -84,6 +92,7 @@ public class MonController {
 
     @FXML
     private void initialize() {
+        criteres.getItems().addAll(TypeCout.values());
         plateforme = new Plateforme();
         graphe = new MultiGrapheOrienteValue();
         boutonAccueil.setOnAction(event -> switchPane(pageAccueil));
@@ -91,6 +100,8 @@ public class MonController {
         boutonHistorique.setOnAction(event -> switchPane(pageHistorique));
         boutonProfil.setOnAction(event -> switchPane(pageProfil));
         boutonChercherVoyage.setOnAction(event -> switchPane(pageVoyage));
+        boutonCalculerVoyage.setOnAction(event -> calculerVoyage());
+        saveProfil.setOnAction(event -> createUser());
         try {
             String data[] = plateforme.getDataFromCSV(trajets);
             plateforme.verifiyData(data);
@@ -101,6 +112,7 @@ public class MonController {
             String[] correspondances = plateforme.getDataFromCSV(couts);
             plateforme.verifiyData(correspondances);
             plateforme.ajouterCorrespondances(correspondances);
+            ajouterVilles();
         } 
         catch (FileNotFoundException e) {System.err.println(e.getMessage());}
         catch (IllegalArgumentException e) {System.err.println("Argument invalide");}
@@ -110,6 +122,45 @@ public class MonController {
     private void switchPane(Pane pane) {
         stackPane.getChildren().forEach(child -> child.setVisible(false));
         pane.setVisible(true);
+    }
+
+    private void calculerVoyage() {
+        graphe = new MultiGrapheOrienteValue();
+        plateforme.ajouterVillesEtTrajets(graphe, criteres.getSelectionModel().getSelectedItem(), getSelectedTransports());
+        List<Chemin> result = AlgorithmeKPCC.kpcc(graphe, villeDepart.getSelectionModel().getSelectedItem(), villeArrivee.getSelectionModel().getSelectedItem(), 3);
+        try {
+            System.out.println(plateforme.afficherPCC(result, criteres.getSelectionModel().getSelectedItem()));
+        } catch (NoTripException e) {System.err.println(e.getMessage());}
+    }
+
+    private ArrayList<ModaliteTransport> getSelectedTransports() {
+        ArrayList<ModaliteTransport> transports = new ArrayList<>();
+        if (checkBoxAvion.isSelected()) transports.add(ModaliteTransport.AVION);
+        if (checkBoxBus.isSelected()) transports.add(ModaliteTransport.BUS);
+        if (checkBoxTrain.isSelected()) transports.add(ModaliteTransport.TRAIN);
+        return transports;
+    }
+
+    private void ajouterVilles() {
+        villeArrivee.getItems().addAll(plateforme.getVilles());
+        villeDepart.getItems().addAll(plateforme.getVilles());
+        FXCollections.sort(villeDepart.getItems(), Comparator.comparing(v -> v.getNom()));
+        FXCollections.sort(villeArrivee.getItems(), Comparator.comparing(v -> v.getNom()));
+    }
+
+    private void createUser() {
+        String name = nom.getText() + " " + prenom.getText();
+        ArrayList<ModaliteTransport> transports = new ArrayList<>();
+        if (profilCheckBoxAvion.isSelected()) transports.add(ModaliteTransport.AVION);
+        if (profilCheckBoxBus.isSelected()) transports.add(ModaliteTransport.BUS);
+        if (profilCheckBoxTrain.isSelected()) transports.add(ModaliteTransport.TRAIN);
+        try {
+            double maxCost = Double.parseDouble(coutMax.getText());
+            voyageur = new Voyageur(name, TypeCout.TEMPS, maxCost, transports);
+            System.out.println("Hello "+voyageur.getNom()+" !");
+        }
+        catch (NumberFormatException e) {System.err.println(e.getMessage());}
+        catch (NullPointerException e) {System.err.println(e.getMessage());}
     }
 }
 
